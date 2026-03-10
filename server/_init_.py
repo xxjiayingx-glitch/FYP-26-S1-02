7c0163c99997144bc87e66e4e35a7fedff7f2302
BYKS48 <benjaminyangks@gmail.com>
2026-03-10 19:58:33 +0800

    Delete _init_.py

--- server/_init_.py
+++
@@ -1,7 +0,0 @@
-from .TrendingNewsCTL import *
-from .SubscriptionCTL import *
-from .FeaturesCTL import *
-from .TestimonialsCTL import *
-from .ViewCompanyProfileCTL import *
-from .registerAccountCTL import *
-from flask_mail import Mail, Message
