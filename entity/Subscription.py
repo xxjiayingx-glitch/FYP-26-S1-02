2971ae613fde43a971c4ee9ac2b952acd7b61762
jyleong <xxjiayingx@gmail.com>
2026-03-12 23:39:01 +0800

    Update Subscription.py

--- entity/Subscription.py
+++ entity/Subscription.py
@@ -1,6 +1,13 @@
 from entity.db_connection import get_db_connection
 
 class Subscription:
+    def get_subscription(userID):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT * FROM Subscription WHERE userID=%s"
+    cursor.execute(query,(userID,))
+    return cursor.fetchone()
+    
     @staticmethod
     def get_active_subscriptions():
         conn = get_db_connection()
