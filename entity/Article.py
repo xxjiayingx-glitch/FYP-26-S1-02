d30627cc29b2e4526d8caaf9cf7e1d674ee01fe0
jyleong <xxjiayingx@gmail.com>
2026-03-12 23:32:51 +0800

    Update Article.py entity

--- entity/Article.py
+++ entity/Article.py
@@ -1,6 +1,33 @@
 from entity.db_connection import get_db_connection
 
 class Article:
+    def get_all_articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Article")
+        articles = cursor.fetchall()
+        conn.close()
+        return articles
+
+    def search_articles(self,keyword):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        sql = "SELECT * FROM Article WHERE articleTitle LIKE %s"
+        cursor.execute(sql,('%'+keyword+'%',))
+        articles = cursor.fetchall()
+        conn.close()
+        return articles
+
+    def get_article(self,articleID):
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        sql="SELECT * FROM Article WHERE articleID=%s"
+        cursor.execute(sql,(articleID,))
+        article = cursor.fetchone()
+        conn.close()
+        return article
+    
     @staticmethod
     def get_total_articles():
         conn = get_db_connection()
