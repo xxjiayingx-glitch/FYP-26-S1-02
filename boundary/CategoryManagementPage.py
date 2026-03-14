43d45a9aeb7cc26e0ed21389b8812d07d5c92296
melisachen518-gif <melisachen518@gmail.com>
2026-03-15 00:45:13 +0800

    Delete boundary/CategoryManagementPage.py

--- boundary/CategoryManagementPage.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, session, redirect, url_for
-
-category_management_bp = Blueprint("category_management", __name__)
-
-@category_management_bp.route("/category-management")
-def category_management_page():
-    if "userID" not in session:
-        return redirect(url_for("login"))
-    return render_template("CategoryManagementMain.html")
