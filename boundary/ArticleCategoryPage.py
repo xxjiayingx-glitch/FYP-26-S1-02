ea91fd6595d60ead6f3bfb32d6742f51b002ec02
melisachen518-gif <melisachen518@gmail.com>
2026-03-15 00:44:53 +0800

    Delete boundary/ArticleCategoryPage.py

--- boundary/ArticleCategoryPage.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, session, redirect, url_for
-
-article_category_page_bp = Blueprint("article_category_page", __name__)
-
-@article_category_page_bp.route("/article-category")
-def article_category_page():
-    if "userID" not in session:
-        return redirect(url_for("login"))
-    return render_template("ArticleCategory.html")
