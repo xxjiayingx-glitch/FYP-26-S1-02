35bb44a8df46abeb2dcbaa9a0a27746540b4cb9d
melisachen518-gif <melisachen518@gmail.com>
2026-03-15 01:17:00 +0800

    Update EditSubscriptionPlansPage.py

--- boundary/EditSubscriptionPlansPage.py
+++ boundary/EditSubscriptionPlansPage.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 
-edit_company_profile_bp = Blueprint("edit_company_profile", __name__)
+edit_subscription_plans_bp = Blueprint("edit_subscription_plans", __name__)
 
-@edit_company_profile_bp.route("/admin/edit-company-profile")
-def edit_company_profile_page():
+@edit_subscription_plans_bp.route("/admin/edit-subscription-plans")
+def edit_subscription_plans_page():
     if "userID" not in session:
         return redirect(url_for("login"))
-    return render_template("EditCompanyProfile.html")
+    return render_template("EditSubscriptionPlans.html")
