430906c6ea7b236e31fbfa8c91ba9bacbf720828
BYKS48 <benjaminyangks@gmail.com>
2026-03-15 20:50:45 +0800

    Update HomePage.py

--- boundary/HomePage.py
+++ boundary/HomePage.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
-home_bp = Blueprint('home', __name__)
+home_bp = Blueprint("home", __name__)
 
-@home_bp.route("/home")
+
+@home_bp.route("/")
 def unreg_home():
+
+    user_type = session.get("userType")
+
+    if user_type is None:
+        return render_template("Unregistered/UnregHome.html")
+
+    if user_type == "free":
+        return render_template("free_homepage.html")
+
+    if user_type == "premium":
+        return render_template("premium_homepage.html")
+
     return render_template("Unregistered/UnregHome.html")
