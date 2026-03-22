ad545f7cddafa7278b88a1609c9e7f8b8c9656ca
melisachen518-gif <melisachen518@gmail.com>
2026-03-23 01:22:34 +0800

    Update ArticleCategoryPage.py

--- boundary/ArticleCategoryPage.py
+++ boundary/ArticleCategoryPage.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 
-article_category_page_bp = Blueprint("article_category_page", __name__)
+article_category_page_bp = Blueprint(
+    "article_category_page",
+    __name__,
+    template_folder="../templates"
+)
 
-@article_category_page_bp.route("/admin/article-category")
+@article_category_page_bp.route("/admin/article-category-page")
 def article_category_page():
+
     if "userID" not in session:
         return redirect(url_for("login"))
+
     return render_template("ArticleCategory.html")
