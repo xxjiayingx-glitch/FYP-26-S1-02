d31697e82694c73ffb28d60087a46655dbbdd599
BYKS48 <benjaminyangks@gmail.com>
2026-03-25 11:25:16 +0800

    update for subscription

--- boundary/UnregSubscriptionPage.py
+++ boundary/UnregSubscriptionPage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for
 from control.UnregisteredUser.SubscriptionCTL import SubscriptionCTL
 
 unregSub_bp = Blueprint("unregsub", __name__)
@@ -6,6 +6,13 @@ subscriptionCTL = SubscriptionCTL()
 
 @unregSub_bp.route("/guestsubscription")
 def unreg_sub():
+    user_type = session.get('userType', '').lower()
+    if "free" in user_type:
+        return render_template("free_homepage.html")
+
+    if "premium" in user_type:
+        return render_template("premium_homepage.html")
+    
     result = subscriptionCTL.getSubscriptionPlans()
 
     if not result["success"]:
